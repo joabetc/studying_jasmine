2313bbb8623c908abb3c601d24e3670a8062f860
Joabe Costa <joabetc@hotmail.com>
2017-12-12 21:57:04 -0200

    Ajusting class to match the test expect

--- src/Scheduling.js
+++ src/Scheduling.js
@@ -3,8 +3,12 @@ function Scheduling() {
   var clazz = {
 
     to: function(appointment) {
-      var twentyDaysInMilliseconds = 1000 * 60 * 60 * 24 * 20;
+      var oneDayInMilliseconds = 1000 * 60 * 60 * 24;
+      var twentyDaysInMilliseconds = oneDayInMilliseconds * 20;
       var newDate = new Date(appointment.getDate().getTime() + twentyDaysInMilliseconds);
+
+      while(newDate.getDay() == 0 || newDate.getDay() == 6)
+        newDate = new Date(newDate.getTime() + oneDayInMilliseconds);
       var newAppointment = new MedicalAppointment(appointment.getName(), appointment.getProcedures(), appointment.isPrivate(), true, newDate);
       return newAppointment;
     }
