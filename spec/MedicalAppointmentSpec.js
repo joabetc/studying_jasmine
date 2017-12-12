c9da7cfd844b79cb93da6b011c6d859a236e2410
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-12-12 12:37:30 -0200

    Fixing test spec for private medical appointments

--- spec/MedicalAppointmentSpec.js
+++ spec/MedicalAppointmentSpec.js
@@ -14,11 +14,11 @@ describe("MedicalAppointment", function() {
     expect(medicalAppointment.price()).toEqual(50);
   });
 
-  it("should charge 2 per private medical appointment", function() {
+  it("should charge the double price per private medical appointment", function() {
     var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, ["proc1", "proc2"], true, false);
 
-    expect(medicalAppointment.price()).toEqual(52);
+    expect(medicalAppointment.price()).toEqual(100);
   });
 
   it("should charge a specific price depending on the procedure", function() {
