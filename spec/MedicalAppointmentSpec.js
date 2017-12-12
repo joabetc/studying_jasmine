b0e2e4ac236a4a63d5384b71f0407aa0e3053a9d
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-12-12 12:54:10 -0200

    Adding beforeEach in the Spec file

--- spec/MedicalAppointmentSpec.js
+++ spec/MedicalAppointmentSpec.js
@@ -1,35 +1,36 @@
 describe("MedicalAppointment", function() {
 
+  var patient;
+  
+  beforeEach(function() {
+    patient = new Patient("Willian", 28, 72, 1.80);
+  });
+  
   it("should not charge if it is a returning appointment", function() {
-    var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, [], true, true);
 
     expect(medicalAppointment.price()).toEqual(0);
   });
 
   it("should charge 25 per medical appointment", function() {
-    var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, ["proc1", "proc2"], false, false);
 
     expect(medicalAppointment.price()).toEqual(50);
   });
 
   it("should charge the double price per private medical appointment without special procedures", function() {
-    var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, ["proc1", "proc2"], true, false);
 
     expect(medicalAppointment.price()).toEqual(100);
   });
 
   it("should charge the double price per private medical appointment with special procedures", function() {
-    var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, ["x-ray"], true, false);
 
     expect(medicalAppointment.price()).toEqual(110);
   });
 
   it("should charge a specific price depending on the procedure", function() {
-    var patient = new Patient("Willian", 28, 72, 1.80);
     var medicalAppointment = new MedicalAppointment(patient, ["common-procedure", "x-ray", "common-procedure", "plaster"], false, false);
 
     expect(medicalAppointment.price()).toEqual(25 + 55 + 25 + 32);
