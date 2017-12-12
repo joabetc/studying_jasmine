760e700189e79486eb3fd43e1b30d2a6a036f288
Joabe Costa <joabetc@hotmail.com>
2017-12-12 00:36:03 -0200

    Creating a new class and its spec

---
+++ spec/MedicalAppointmentSpec.js
@@ -0,0 +1,30 @@
+describe("MedicalAppointment", function() {
+
+  it("should not charge if it is a returning appointment", function() {
+    var patient = new Patient("Willian", 28, 72, 1.80);
+    var medicalAppointment = new MedicalAppointment(patient, [], true, true);
+
+    expect(medicalAppointment.price()).toEqual(0);
+  });
+
+  it("should charge 25 per medical appointment", function() {
+    var patient = new Patient("Willian", 28, 72, 1.80);
+    var medicalAppointment = new MedicalAppointment(patient, ["proc1", "proc2"], false, false);
+
+    expect(medicalAppointment.price()).toEqual(50);
+  });
+
+  it("should charge 2 per private medical appointment", function() {
+    var patient = new Patient("Willian", 28, 72, 1.80);
+    var medicalAppointment = new MedicalAppointment(patient, ["proc1", "proc2"], true, false);
+
+    expect(medicalAppointment.price()).toEqual(52);
+  });
+
+  it("should charge a specific price depending on the procedure", function() {
+    var patient = new Patient("Willian", 28, 72, 1.80);
+    var medicalAppointment = new MedicalAppointment(patient, ["common-procedure", "x-ray", "common-procedure", "plaster"], false, false);
+
+    expect(medicalAppointment.price()).toEqual(25 + 55 + 25 + 32);
+  });
+});
---
+++ src/MedicalAppointment.js
@@ -0,0 +1,22 @@
+function MedicalAppointment(patient, procedures, private, returning) {
+
+  var clazz = {
+    price: function() {
+      if(returning) return 0;
+
+      var finalPrice = 0;
+
+      procedures.forEach(function(procedure) {
+        if("x-ray" == procedure) finalPrice += 55;
+        else if("plaster" == procedure) finalPrice += 32;
+        else finalPrice += 25;
+      });
+
+      if(private) finalPrice += 2;
+
+      return finalPrice;
+    }
+  };
+
+  return clazz;
+};
