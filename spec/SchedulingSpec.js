27db2e3814b616e14f5139759a72150884424a80
Joabe Costa <joabetc@hotmail.com>
2017-12-12 21:53:17 -0200

    Creating a new spec for not scheduling during the weekend

--- spec/SchedulingSpec.js
+++ spec/SchedulingSpec.js
@@ -8,5 +8,15 @@ describe("Scheduling", function() {
     var newAppointment = schedule.to(appointment);
 
     expect(newAppointment.getDate().toString()).toEqual(new Date(2014, 7, 21).toString());
+  });
+
+  it("should not schedule during the weekend", function() {
+    var patient = new PatientBuilder().build();
+    var schedule = new Scheduling();
+
+    var appointment = new MedicalAppointment(patient, [], false, false, new Date(2014, 5, 30));
+    var newAppointment = schedule.to(appointment);
+
+    expect(newAppointment.getDate().toString()).toEqual(new Date(2014, 6, 21).toString());
   })
 })
